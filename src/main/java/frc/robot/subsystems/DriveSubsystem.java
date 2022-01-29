bc5e41f0ab37af1521e8dbb888491cf50d063dd9
Quin <gboumstein@gmail.com>
2022-01-29 10:31:16 -0600

    Update to 2022

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -10,21 +10,20 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class DriveSubsystem extends SubsystemBase {
+  private Encoder encoder1;
 
   private WPI_TalonSRX talon1 = new WPI_TalonSRX(14);//left support
   private WPI_TalonSRX talon2 = new WPI_TalonSRX(10);//left main
   private WPI_TalonSRX talon3 = new WPI_TalonSRX(15);// right main
   private WPI_TalonSRX talon4 = new WPI_TalonSRX(11);//right slave not main
 
-  private SpeedControllerGroup left = new SpeedControllerGroup(talon1, talon2);
-  private SpeedControllerGroup right = new SpeedControllerGroup(talon3, talon4);
+  private MotorControllerGroup left = new MotorControllerGroup(talon1, talon2);
+  private MotorControllerGroup right = new MotorControllerGroup(talon3, talon4);
 
   private DifferentialDrive drive = new DifferentialDrive(left, right);
 
@@ -41,11 +40,14 @@ public class DriveSubsystem extends SubsystemBase {
     talon1.setInverted(false);
     talon3.setInverted(false);
 
+    encoder1 = new Encoder(0,1);
+
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     System.out.println("" +  talon1.getSelectedSensorVelocity() + "\n" + talon2.getSelectedSensorVelocity());
+    System.out.println("" + encoder1.get());
   }
 }
